464aa5e0e649038c81560c58ff673e1ccf8d3ce7
Aditya Fakhri Riansyah <adityafakhri03@gmail.com>
2025-05-06 16:02:31 +0700

    Add admin views and routes for order, role, and user management
    - Created new Blade views for editing and listing orders, roles, and users.
    - Implemented a dashboard view for admin with statistics and charts.
    - Added routes for managing categories, items, roles, users, and orders in the web.php file.

---
+++ app/Http/Controllers/itemController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class itemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Fetch all items from the database
+        $items = Item::orderBy('name', 'asc')->get();
+
+        // Return the view with the items
+        return view('admin.item.index', compact('items'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $categories = Category::orderBy('cat_name', 'asc')->get();
+
+        // Return the view to create a new item
+        return view('admin.item.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\itemController;
 use App\Http\Controllers\MenuController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return redirect()->route('menu');
@@ -17,4 +23,15 @@ Route::get('/cart/clear', [MenuController::class, 'clearCart'])->name('cart.clea
 Route::get('/checkout', [MenuController::class, 'checkout'])->name('checkout');
 Route::post('/checkout/store', [MenuController::class, 'storeOrder'])->name('checkout.store');
 Route::get('/checkout/success/{orderId}', [MenuController::class, 'checkoutSuccess'])->name('checkout.success');
+
+
+//admin routes
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('dashboard');
+Route::resource('categories', CategoryController::class);
+Route::resource('items', itemController::class);
+Route::resource('roles', RoleController::class);
+Route::resource('users', UserController::class);
+Route::resource('orders', OrderController::class);
 
