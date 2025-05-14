07a4e3c265acd846fd521dd3843be9b84c0375fa
Aditya Fakhri Riansyah <adityafakhri03@gmail.com>
2025-05-14 15:06:57 +0700

    Refactor User management: implement user creation and editing with validation, success messages, and role selection; enhance user listing with improved layout and action buttons.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Http\Request;
 
@@ -11,7 +14,13 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        // Fetch all users from the database
+        $users = User::whereHas('role', function($query) {
+            $query->where('role_name', '!=', 'customer');
+        })->orderBy('fullname')->get();
+
+        // Return the users to a view
+        return view('admin.user.index', compact('users'));
     }
 
     /**
@@ -19,7 +28,10 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        // Fetch all roles from the database
+        $roles = Role::all();
+        // Return the view to create a new user
+        return view('admin.user.create', compact('roles'));
     }
 
     /**
@@ -27,7 +39,34 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate the request data
+        $validatedData = $request->validate([
+            'fullname' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users,username',
+            'phone' => 'required|string|max:255|unique:users,phone',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+            'role_id' => 'required|exists:roles,id',
+        ], [
+            'fullname.required' => 'The full name is required.',
+            'username.required' => 'The username is required.',
+            'phone.required' => 'The phone number is required.',
+            'email.required' => 'The email address is required.',
+            'password.required' => 'The password is required.',
+            'role_id.required' => 'The role is required.',
+            'password.confirmed' => 'The password confirmation does not match.',
+            'username.unique' => 'The username has already been taken.',
+            'phone.unique' => 'The phone number has already been taken.',
+            'email.unique' => 'The email address has already been taken.',
+        ]);
+
+        // Create a new user
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+        User::create($validatedData);
+
+        // Redirect to the users index with a success message
+        return redirect()->route('users.index')->with('success', 'User created successfully.');
     }
 
     /**
@@ -43,15 +82,50 @@ class UserController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        // Find the user by ID
+        $user = User::findOrFail($id);
+
+        // Fetch all roles from the database
+        $roles = Role::all();
+
+        // Return the view to edit the user
+        return view('admin.user.edit', compact('user', 'roles'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        // Validate the request data
+        $validatedData = $request->validate([
+            'fullname' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users,username,' . $user->id,
+            'phone' => 'required|string|max:15',
+            'email' => 'required|email|unique:users,email,'. $user->id,
+            'password' => ['nullable','string','min:8','confirmed', function($attribute, $value, $fail) use ($user) {
+                    if(Hash::check($value, $user->password)) {
+                        $fail("Password baru tidak boleh sama dengan password lama");
+                    }
+                },
+            ],
+            'role_id' => 'required|exists:roles,id',
+        ], [
+            'fullname.required' => 'The full name is required.',
+            'username.required' => 'The username is required.',
+            'phone.required' => 'The phone number is required.',
+            'email.required' => 'The email address is required.',
+            'role_id.required' => 'The role is required.',
+            'password.confirmed' => 'The password confirmation does not match.'
+        ]);
+
+        // Create a new user
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+        $user->update($validatedData);
+
+        // Redirect to the users index with a success message
+        return redirect()->route('users.index')->with('success', 'User updated successfully.');
     }
 
     /**
@@ -59,6 +133,13 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        // Find the role by ID
+        $user = User::findOrFail($id);
+
+        // Delete the role
+        $user->delete();
+
+        // Redirect to the roles index with a success message
+        return redirect()->route('users.index')->with('success', 'User deleted successfully.');
     }
 }
